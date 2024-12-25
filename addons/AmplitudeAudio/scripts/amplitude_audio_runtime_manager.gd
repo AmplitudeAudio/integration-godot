461597ef18ee9d027c3dcb0fb9dd6e862db5f066
Axel Nana <axel.nana@aliens-group.com>
2024-12-25 18:03:10 +0100

    feat: Restructure project.

--- addons/AmplitudeAudio/scripts/amplitude_audio_runtime_manager.gd
+++ addons/AmplitudeAudio/scripts/amplitude_audio_runtime_manager.gd
@@ -1,3 +1,12 @@
 extends Node
 
 var suspend_on_focus_loss: bool = false;
+
+func _init() -> void:
+	suspend_on_focus_loss = ProjectSettings.get_setting("amplitude_audio/user_settings/suspend_on_focus_loss", false)
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	Amplitude.init()
+	
+func _process(delta: float) -> void:
+	if Amplitude.is_initialized():
+		Amplitude.advance_frame(delta)
